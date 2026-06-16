6867b58bdb917a474fa3fc48597c7bb59dc66576
andreyabpaiva <andreyacbpaiva@gmail.com>
2026-06-16 20:04:58 -0300

    fix: tailwind - layout miscommunication

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Portal de TCCs — Computação UFPA",
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang="pt-BR">
